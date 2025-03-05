cebea55043d66e8599c0f20f3dbe0a9e9d76900c
tylergames42 <51301871+tylergames42@users.noreply.github.com>
2025-03-04 21:29:33 -0600

    Revert "Updated to 1.3.0"
    This reverts commit b2720604ad94e12c6426a627ae7338fa5a2bea5c.

--- overrides/kubejs/client_scripts/tooltips.js
+++
@@ -1,7 +0,0 @@
-// priority: 0
-
-ItemEvents.tooltip(event => {
-  event.add('minecraft:name_tag', '§8[§7Use§8] to rename')
-  
-  event.add(['minecraft:fire_charge', 'supplementaries:bomb', 'supplementaries:bomb_blue', 'savage_and_ravage:creeper_spores', 'caverns_and_chasms:kunai', 'environmental:mud_ball', 'minecraft:red_dye', 'minecraft:orange_dye', 'minecraft:yellow_dye', 'minecraft:green_dye', 'minecraft:lime_dye', 'minecraft:blue_dye', 'minecraft:light_blue_dye', 'minecraft:cyan_dye', 'minecraft:purple_dye', 'minecraft:pink_dye', 'minecraft:magenta_dye', 'minecraft:black_dye', 'minecraft:gray_dye', 'minecraft:light_gray_dye', 'minecraft:white_dye', 'minecraft:brown_dye'], '§7§oThrowable')
-})
